1db2aff9a6429503e591b403f6266acc2f6d1a8b
Mukui012 <spencermukui@gmail.com>
2023-07-21 20:43:34 +0000

    function that sorts a doubly linked list of integers using the insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,8 +1,8 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - function that sorts a doubly linked list of integers in
- * ascending order using the Insertion sort algorithm
+ * insertion_sort_list - function that sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
  * @list: double pointer to head of the linked list
  *
  * Return: void
